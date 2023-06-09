fe3e0fd56ad3c26363a49aa991f0fe135dab2deb
chihatw <chiha.tw@gmail.com>
2023-06-09 20:55:56 +0900

    update node version

--- src/application/0-store/middleware.ts
+++ src/application/0-store/middleware.ts
@@ -1,4 +1,3 @@
-// dummy
 import user from '../user/framework/1-middleware';
 import audio from '../audio/framework/1-middleware';
 import recordWorkouts from '../recordWorkouts/framework/1-middleware';
